b564d71f448f49767b6c0cf38ddbfcbde95db908
nobodysGitHub <91459536+nobodysGitHub@users.noreply.github.com>
2023-01-30 10:36:56 +0100

    cubeSpawner

--- src/genesis/components/audio/sounds.ts
+++ src/genesis/components/audio/sounds.ts
@@ -5,7 +5,8 @@ import { AudioManager } from "./audio.manager"
 const lampClip = new AudioClip("assets/audio/v3/Lamp.wav")
 
 const lamp1 = new Entity()
-lamp1.addComponent(new Transform({position: new Vector3(124.85, 75.29, 123.76)}))
+const lamp1Transform = new Transform({position: new Vector3(124.85, 75.29, 123.76)})
+lamp1.addComponent(lamp1Transform)
 const surce = new AudioSource(lampClip)
 lamp1.addComponent(surce)
 surce.loop = true
@@ -15,7 +16,8 @@ engine.addEntity(lamp1)
 
 
 const lamp2 = new Entity()
-lamp2.addComponent(new Transform({position: new Vector3(136.87, 70.57, 149.46)}))
+const lamp2Transform = new Transform({position: new Vector3(136.87, 70.57, 149.46)})
+lamp2.addComponent(lamp2Transform)
 const surce2 = new AudioSource(lampClip)
 lamp2.addComponent(surce2)
 surce2.loop = true
@@ -25,7 +27,8 @@ engine.addEntity(lamp2)
 
 
 const lamp3 = new Entity()
-lamp3.addComponent(new Transform({position: new Vector3(159.85, 67.76, 161.82)}))
+const lamp3Transform = new Transform({position: new Vector3(159.85, 67.76, 161.82)})
+lamp3.addComponent(lamp3Transform)
 const surce3 = new AudioSource(lampClip)
 lamp3.addComponent(surce3)
 surce3.loop = true
@@ -35,7 +38,8 @@ engine.addEntity(lamp3)
 
 
 const lamp4 = new Entity()
-lamp4.addComponent(new Transform({position: new Vector3(164.19, 66.77, 137.21)}))
+const lamp4Transform = new Transform({position: new Vector3(164.19, 66.77, 137.21)})
+lamp4.addComponent(lamp4Transform)
 const surce4 = new AudioSource(lampClip)
 lamp4.addComponent(surce4)
 surce4.loop = true
@@ -44,7 +48,8 @@ surce4.playOnce()
 engine.addEntity(lamp4)
 
 const lamp5 = new Entity()
-lamp5.addComponent(new Transform({position: new Vector3(158.51, 64.78, 108.98)}))
+const lamp5Transform = new Transform({position: new Vector3(158.51, 64.78, 108.98)})
+lamp5.addComponent(lamp5Transform)
 const surce5 = new AudioSource(lampClip)
 lamp5.addComponent(surce5)
 surce5.loop = true
@@ -54,7 +59,8 @@ engine.addEntity(lamp5)
 
 
 const lamp6 = new Entity()
-lamp6.addComponent(new Transform({position: new Vector3(158.51, 64.78, 108.98)}))
+const lamp6Transform = new Transform({position: new Vector3(158.51, 64.78, 108.98)})
+lamp6.addComponent(lamp6Transform)
 const surce6 = new AudioSource(lampClip)
 lamp6.addComponent(surce6)
 surce6.loop = true
@@ -68,7 +74,8 @@ engine.addEntity(lamp6)
 const windOnLEafClip = new AudioClip("assets/audio/v3/WindOnLeafs.mp3")
 
 const tree1 = new Entity()
-tree1.addComponent(new Transform({position: new Vector3(168.2958, 65.22231, 140.32)}))
+const tree1Transform = new Transform({position: new Vector3(168.2958, 70, 140.32)})
+tree1.addComponent(tree1Transform)
 const treeSource = new AudioSource(windOnLEafClip)
 tree1.addComponent(treeSource)
 treeSource.loop = true
@@ -77,18 +84,31 @@ treeSource.pitch = 0.9
 treeSource.playOnce()
 engine.addEntity(tree1)
 
-const tree6 = new Entity()
-tree6.addComponent(new Transform({position: new Vector3(195.69, 65.59, 131.08)}))
-const treeSource6 = new AudioSource(windOnLEafClip)
-tree6.addComponent(treeSource6)
-treeSource6.loop = true
-treeSource6.volume = 0.03
+const tree2 = new Entity()
+const tree2Transform = new Transform({position: new Vector3(195.69, 65.59, 131.08)})
+tree2.addComponent(tree2Transform)
+const treeSource2 = new AudioSource(windOnLEafClip)
+tree2.addComponent(treeSource2)
+treeSource2.loop = true
+treeSource2.volume = 0.03
 treeSource.pitch = 0.9
-treeSource6.playOnce()
-engine.addEntity(tree6)
+treeSource2.playOnce()
+engine.addEntity(tree2)
+
+const tree3 = new Entity()
+const tree3Transform = new Transform({position: new Vector3(130, 80, 115)})
+tree3.addComponent(tree3Transform)
+const treeSource3 = new AudioSource(windOnLEafClip)
+tree3.addComponent(treeSource3)
+treeSource3.loop = true
+treeSource3.volume = 0.03
+treeSource.pitch = 0.9
+treeSource3.playOnce()
+engine.addEntity(tree3)
 
 const tree4 = new Entity()
-tree4.addComponent(new Transform({position: new Vector3(142, 75.59, 152.08)}))
+const tree4Transform = new Transform({position: new Vector3(142, 75.59, 152.08)})
+tree4.addComponent(tree4Transform)
 const treeSource4 = new AudioSource(windOnLEafClip)
 tree4.addComponent(treeSource4)
 treeSource4.loop = true
@@ -98,7 +118,8 @@ treeSource4.playOnce()
 engine.addEntity(tree4)
 
 const tree5 = new Entity()
-tree5.addComponent(new Transform({position: new Vector3(86, 85.59, 152.08)}))
+const tree5Transform = new Transform({position: new Vector3(86, 85.59, 152.08)})
+tree5.addComponent(tree5Transform)
 const treeSource5 = new AudioSource(windOnLEafClip)
 tree5.addComponent(treeSource5)
 treeSource5.loop = true
@@ -107,15 +128,6 @@ treeSource.pitch = 0.9
 treeSource5.playOnce()
 engine.addEntity(tree5)
 
-const tree7 = new Entity()
-tree7.addComponent(new Transform({position: new Vector3(130, 80, 115)}))
-const treeSource7 = new AudioSource(windOnLEafClip)
-tree7.addComponent(treeSource7)
-treeSource7.loop = true
-treeSource7.volume = 0.03
-treeSource.pitch = 0.9
-treeSource7.playOnce()
-engine.addEntity(tree7)
 
 //#endregion 
 
@@ -123,7 +135,8 @@ engine.addEntity(tree7)
 const floatingRoksClip = new AudioClip("assets/audio/v3/FloatingRoks.mp3")
 
 const rock = new Entity()
-rock.addComponent(new Transform({position: new Vector3(190, 65.59, 140)}))
+const rock1Transform = new Transform({position: new Vector3(190, 65.59, 140)})
+rock.addComponent(rock1Transform)
 const rockSource = new AudioSource(floatingRoksClip)
 rock.addComponent(rockSource)
 rockSource.loop = true
@@ -133,7 +146,8 @@ rockSource.playOnce()
 engine.addEntity(rock)
 
 const rock2 = new Entity()
-rock2.addComponent(new Transform({position: new Vector3(160, 70, 175)}))
+const rock2Transform = new Transform({position: new Vector3(160, 70, 175)})
+rock2.addComponent(rock2Transform)
 const rockSource2 = new AudioSource(floatingRoksClip)
 rock2.addComponent(rockSource2)
 rockSource2.loop = true
@@ -203,7 +217,8 @@ export function activatePillarSound4(pillarEntity:Entity){
 const generatorsClip = new AudioClip("assets/audio/v3/NotWorkingGenerators.mp3")
 
 const generators = new Entity()
-generators.addComponent(new Transform({position: new Vector3(101, 79, 140)}))
+const generatorsTransform = new Transform({position: new Vector3(101, 79, 140)})
+generators.addComponent(generatorsTransform)
 const generatorSource = new AudioSource(generatorsClip)
 generators.addComponent(generatorSource)
 generatorSource.loop = true
@@ -229,11 +244,12 @@ export function changeGeneratosSound(){
 
 //#region Portals
 const portalClip = new AudioClip("assets/audio/v3/Pillar.mp3")
+const portalsTransform = new Transform({position: new Vector3(100, 95, 95)})
 
 export function activateInitialSoundPortal(){
     
     const portals = new Entity()
-    portals.addComponent(new Transform({position: new Vector3(100, 95, 95)}))
+    portals.addComponent(portalsTransform)
     AudioManager.instance().playOnce("tower_charge", { volume: 1, pitch: 0.3, parent: portals })
     const portalSource = new AudioSource(portalClip)
     portals.addComponent(portalSource)
@@ -256,3 +272,23 @@ export function activateLoopSoundPortal(){
     engine.addEntity(portals)
 }
 //#endregion
+
+let cubeDebuggerEnable = true
+const soundsTransform = [lamp1Transform, lamp2Transform, lamp3Transform, lamp4Transform, lamp5Transform, lamp6Transform, tree1Transform, tree2Transform, tree3Transform, tree4Transform, tree5Transform, rock1Transform, rock2Transform, generatorsTransform, portalsTransform]
+cubeSpawner()
+
+function cubeSpawner(){
+    if(cubeDebuggerEnable){
+        for (let i = 0; i < soundsTransform.length; i++) {
+            //create an entity 
+            //add a cube
+            //put in the position of the transform 
+            // add to engine 
+            
+            const a = new Entity()
+            a.addComponent(soundsTransform[i]) 
+            a.addComponent(new BoxShape())
+            engine.addEntity(a)
+        }
+    }
+}
