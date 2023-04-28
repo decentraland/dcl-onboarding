ed938e128308a1803df444d761bd84ca9aec9d4f
nobodysGitHub <91459536+nobodysGitHub@users.noreply.github.com>
2023-04-28 17:22:22 +0800

    fix

--- src/jsonData/textsTutorialPopups.ts
+++ src/jsonData/textsTutorialPopups.ts
@@ -32,10 +32,10 @@ export const THIRD_ISLAND_5 = "Come back here! That’s everything I need."
 
 export const THIRD_ISLAND_6 = "Thanks for collecting the materials and meshes, now I can make more wearables."
 export const THIRD_ISLAND_7 = "Your next task is to talk to Kit, find him on the last island."
-export const THIRD_ISLAND_8 = CONFIG.CLAIM_NONWEB3_SHOW_DISCLAIMER.emote ? "Thanks for your help." : "Before you go, take this wearable as thanks for your help."
+export const THIRD_ISLAND_8 = CONFIG.CLAIM_NONWEB3_SHOW_DISCLAIMER.material ? "Thanks for your help." : "Before you go, take this wearable as thanks for your help."
 export const THIRD_ISLAND_9 = "Kit should be in the next island."
 export const THIRD_ISLAND_10 = "Kit should be in the next island."
-export const THIRD_ISLAND_11 = CONFIG.CLAIM_NONWEB3_SHOW_DISCLAIMER.emote ? "Whith the materials that you gave me, I can create awesome wearables. Thanks!" : "Oh! You forgot your wearable."
+export const THIRD_ISLAND_11 = CONFIG.CLAIM_NONWEB3_SHOW_DISCLAIMER.material ? "Whith the materials that you gave me, I can create awesome wearables. Thanks!" : "Oh! You forgot your wearable."
 
 // Dialogs  Kit island
 export const FOURTH_ISLAND_0 = "Hi I’m Kit. You’re just about ready to visit the main part of Decentraland: Genesis City!"
@@ -54,7 +54,7 @@ export const PORTAL_ISLAND_0 = "Awesome, the portals are online!"
 export const PORTAL_ISLAND_1 = "From here on out it's up to you... <b>what do you want to do in Decentraland?</b>"
 export const PORTAL_ISLAND_2 = "Don't like these choices? Press <b>tab</b> to open the menu and choose from loads of different experiences."
 export const PORTAL_ISLAND_3 = "Well, what are you waiting for, choose your next experience!"
-export const PORTAL_ISLAND_4 = CONFIG.CLAIM_NONWEB3_SHOW_DISCLAIMER.emote ? "Thanks again for activating the portals" : "Before you go, don't forget your reward for activating the portals"
+export const PORTAL_ISLAND_4 = CONFIG.CLAIM_NONWEB3_SHOW_DISCLAIMER.puzzle ? "Thanks again for activating the portals" : "Before you go, don't forget your reward for activating the portals"
 
 
 
